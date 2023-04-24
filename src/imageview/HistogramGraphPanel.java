c416934f01589d8b57d1377febb516c7d2a7f84a
Aryan Jain <aryanjn807@gmail.com>
2023-04-24 15:43:40 -0400

    fixed list errors

--- src/imageview/HistogramGraphPanel.java
+++ src/imageview/HistogramGraphPanel.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.*;
+import javax.swing.JPanel;
 
 /**
  * This class represents the panel that displays the histogram of the image. It extends the JPanel
